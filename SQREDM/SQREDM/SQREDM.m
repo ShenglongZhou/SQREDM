2bccd0274de9fa044ea122b023ac7ba20770c7f0
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2018-05-09 11:33:15 +0100

    Update SQREDM.m

--- SQREDM/SQREDM/SQREDM.m
+++ SQREDM/SQREDM/SQREDM.m
@@ -228,7 +228,7 @@ function  [m,itmax,Eigtol,Objtol] = getparameters(n,pars)
     itmax  = 2000; 
     m      = 0;
     Objtol = sqrt(n)*1e-3;
-    Eigtol = 1e-3;              % Eigtol = 1e-2 if the noise factor nf>0.2 
+    Eigtol = 1e-2;   
     if isfield(pars, 'Otol');   Objtol=pars.Otol;  end
     if isfield(pars, 'm');      m=pars.m;          end
     if isfield(pars, 'Etol');   Eigtol=pars.Etol;  end
