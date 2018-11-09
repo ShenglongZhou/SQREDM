837d5b53dfd0ead3c6cdd7faab71e6eeaf2e27cd
Shenglong Zhou <36698633+ShenglongZhou@users.noreply.github.com>
2018-11-09 12:50:12 +0000

    Update SQREDM.m

--- SQREDM/SQREDM/SQREDM.m
+++ SQREDM/SQREDM/SQREDM.m
@@ -310,8 +310,9 @@ end
 function Z0= ProjKr(A,r)
 % The projection of A on cone K_+^n(r)  
 	JAJ     = JXJ(A);
-	[V0,P0]= eigs((JAJ+JAJ')/2,r,'LA');
-	Z0     = real(V0*max(0,P0)*V0'+A-JAJ); 
+	JAJ     = (JAJ+JAJ')/2
+	[V0,P0] = eigs(JAJ,r,'LA');
+	Z0      = real(V0*max(0,P0)*V0'+A-JAJ); 
 end
 
 % ------------------------------------------------------------------------
